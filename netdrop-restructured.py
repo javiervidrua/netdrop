d2a4e21336d0c762217807fb61c9e318db3ff4d8
javiervidrua <67796321+javiervidrua@users.noreply.github.com>
2021-02-10 16:04:06 +0100

    Made everything work and look nice. Next: Hosts name discovery and selection by name, SSL encryption

--- netdrop-restructured.py
+++ netdrop-restructured.py
@@ -30,7 +30,7 @@ def keyboard_interrupt_handler(signal, frame):
 # Returns the interface of the local network of the machine in CIDR format (e.g 192.168.1.70/24)
 def get_iface(verbose=False):
     # First, we get the IP of the host -> https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
-    if verbose: print('[v] Getting the IP of the machine')
+    if verbose: print('[v] get_iface: Getting the IP of the machine')
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
         err = s.connect(('192.255.255.255', 1)) # Does not even have to be reachable
@@ -50,7 +50,7 @@ def get_iface(verbose=False):
         ip = '127.0.0.1'
     finally:
         s.close()
-        print('[+] Host\'s IP: ' + str(ip))
+        print('[+] get_iface: Host\'s IP is ' + str(ip))
     
     # Second, we get the nics -> https://stackoverflow.com/questions/41420165/get-ipconfig-result-with-python-in-windows/41420850#41420850
     cmd = 'wmic.exe nicconfig where "IPEnabled  = True" get ipaddress,MACAddress,IPSubnet,DNSHostName,Caption,DefaultIPGateway /format:rawxml'
@@ -105,7 +105,7 @@ def get_iface(verbose=False):
     
     # Fourth, we create an object to obtain interface information
     ret = ipaddress.ip_interface(iface)
-    print('[+] Hosts network: ' + str(ret.network))
+    print('[+] get_iface: Hosts network is ' + str(ret.network))
     return ret
 
 
@@ -173,6 +173,81 @@ def network_scanner_fast(ip, netmask, verbose=False): # 192.168.1.0, 24
     return online_hosts
 
 
+# Handles a file send
+def client(server, f):
+    asyncio.get_event_loop().run_until_complete(file_send(server, f))
+
+
+# Sends a file to a server
+async def file_send(server, f):
+    uri = "ws://"+server+":8765"
+    async with websockets.connect(uri) as websocket:
+        # Send the name of the file
+        print('[+] file_send: Sending the filename')
+        buff = str(f)
+        await websocket.send(buff)
+        # Listen for the NACK
+        print('[+] file_send: Waiting for the NACK')
+        buff = await websocket.recv()
+        try:
+            assert buff == 'NACK'
+        except AssertionError:
+            print('[-] file_send: The server did not accept the file share, quitting...')
+            exit(11)
+        # Send the file
+        print('[+] file_send: Sending the file ' + str(f))
+        with open(f,'rb') as file:
+            for line in file:
+                await websocket.send(line)
+        # Send the FACK
+        print('[+] file_send: Sending the FACK and waiting for the EOT')
+        buff = 'FACK'
+        await websocket.send(buff)
+        # Listen for the EOT
+        buff = await websocket.recv()
+        try:
+            assert buff == 'EOT'
+            print('[+] file_send: Received EOT')
+        except AssertionError:
+            print('[-] file_send: The server did not respond with EOT, maybe the file transfer failed')
+            return 12
+
+
+# Handles a file download
+async def file_download(websocket, path):
+    # Wait for a connection
+    # Listen for the name of the file
+    print('[+] file_download: Waiting for the filename')
+    buff = await websocket.recv()
+    filename = str(datetime.now().time()).split('.')[1] + str(buff)    
+
+    # Ask the user for consentment
+    print('[+] file_download: Received filename "' + str(filename) + '" from ' + str(websocket.remote_address[0]) + ' on port ' + str(websocket.remote_address[1]))
+    if 'yes' != input('[+] file_download: To allow the file transfer enter "yes": '):
+        # Send EOT and quit
+        print('[+] file_download: Sending EOT to end the file transfer')
+        buff = 'EOT'
+        await websocket.send(buff)
+    else:
+        # Receive the file
+        print('[+] file_download: Sending NACK to start the file transfer')
+        buff = 'NACK'
+        await websocket.send(buff)
+
+        # Listen for the file and the FACK
+        print('[+] file_download: Waiting for the file and the FACK')
+        with open(filename, "wb+") as file:
+            buff = await websocket.recv()
+            while buff != 'FACK':
+                file.write(buff)
+                buff = await websocket.recv()
+        
+        # Sending the EOT
+        print('[+] file_download: File received, FACK received, sending EOT')
+        buff = 'EOT'
+        await websocket.send(buff)
+        print('[+] file_download: EOT sent')
+
 def main():
     # Register the signal handlers
     signal.signal(signal.SIGINT, keyboard_interrupt_handler)
@@ -184,36 +259,50 @@ def main():
     args = parser.parse_args()
     files = args.file
 
+    # Get the network info
+    iface = get_iface()
+
     # Check if client or server
-    if files:
-        is_client = True
+    if files:        
         # Client
-        if is_client == True:
-            # Check if the files exists and are readable
-            for file in files:
-                is_file_readable = os.path.isfile(file)
-                if is_file_readable == False:
-                    print('[-] netdrop: The input file ' + file + ' is not readable, quitting...')
-                    exit(1)
-            # Get the network info
-            iface = get_iface()
-            # Scan the network
-            hosts = network_scanner_fast(str(iface.network).split('/')[0], str(iface.network).split('/')[1])
-            print('[+] netdrop: Hosts found on the local network: ' + str(hosts))
-            # Ask for a server (receiver of the file)
-            server = ''
-            myregex = "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
-            regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
-            while not re.match(regex, server):
-                server = input("[*] Enter the IP of the end machine: ")
-            # Create process to send each file to the selected server
-            # Ask for more files to send or exit (go to start of client: create function)
-        # Server
+        is_client = True
+        # Check if the files exists and are readable
+        for file in files:
+            is_file_readable = os.path.isfile(file)
+            if is_file_readable == False:
+                print('[-] netdrop: The input file ' + file + ' is not readable, quitting...')
+                exit(1)        
+        # Scan the network
+        hosts = network_scanner_fast(str(iface.network).split('/')[0], str(iface.network).split('/')[1])
+        print('[+] netdrop: Hosts found on the local network: ' + str(hosts))
+        # Ask for a server (receiver of the file)
+        server = ''
+        myregex = "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+        regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
+        while not re.match(regex, server):
+            server = input("[*] Enter the IP of the end machine: ")
+        # Create processes to send each file to the selected server
+        processes = []
+        for file in files:
+            p = Process(target=client, args=(str(server), str(file),))
+            processes.append(p)
+            p.start()
+        # Wait for the processes to finish
+        for p in processes:
+            try:
+                p.join()
+            except Exception as e:
+                if hasattr(e, 'message'):
+                        print('[-] netdrop: Error joining file_send processes: ' + e.message)
+                else:
+                    print('[-] netdrop: Error joining file_send processes: ' + e)        
     else:
+        # Server
         is_client = False
-            # Wait for a connection
-            # Ask the user for consentment
-            # Receive the file
+        # Start the server
+        start_server = websockets.serve(file_download, str(iface.ip), 8765)
+        asyncio.get_event_loop().run_until_complete(start_server)
+        asyncio.get_event_loop().run_forever()
 
     return 0
 
